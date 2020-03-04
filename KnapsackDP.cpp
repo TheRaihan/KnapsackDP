6cfead724aba9f870e8bc5fda59eca2e0527771e
noob <rayhan.taher@gmail.com>
2020-03-05 05:40:08 +0600

    Splited into functions and Comments added

--- KnapsackDP.cpp
+++ KnapsackDP.cpp
@@ -1,35 +1,24 @@
 #include <bits/stdc++.h>
-#define inf 1 / 0.0
-#define fin freopen("input.txt", "r", stdin)
-#define fout freopen("output.txt", "w", stdout)
-#define mem(a, b) memset(a, b, sizeof(a))
-#define err cout << "OK" << endl;
-#define PI 2 * acos(0.0)
-#define F first
-#define S second
-#define gcd(x, y) __gcd(x, y)
-#define lcm(a, b) (a * (b / gcd(a, b)))
-#define MOD 100000007
 using namespace std;
+
+int a, x, t, y, matrix[100][100];
+int n = 4, m = 8;
+int p[] = {0, 1, 2, 5, 6};
+int wt[] = {0, 2, 3, 4, 5};
+
+/*  int n = 4, m = 5;
+    int p[] = {0, 12, 10, 20, 15};
+    int wt[] = {0, 2, 1, 3, 2}; */
+
+void PrintWeights();
 
 int main()
 {
-
-    int a, x, t, y, matrix[100][100];
-    int n = 4, m = 8;
-    int p[] = {0, 1, 2, 5, 6};
-    int wt[] = {0, 2, 3, 4, 5};
-
-    // int n = 4, m = 5;
-    // int p[] = {0, 12, 10, 20, 15};
-    // int wt[] = {0, 2, 1, 3, 2};
-
+    //Initialise 
     for (int j = 0; j < m; j++)
         matrix[0][j] = 0;
-
     for (int i = 0; i < n; i++)
         matrix[i][0] = 0;
- 
    
     for (int i = 1; i <= n; i++)
         for (int w = 1; w <= m; w++)
@@ -40,34 +29,29 @@ int main()
                     else matrix[i][w] = matrix[i - 1][w];
         }
 
-    cout << endl;
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= m; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+    cout << "\nOptimal Solution for this knapsack is " << matrix[n][m] << endl;
 
-    cout << "Optimal Solution for this knapsack is " << matrix[n][m] << endl;
+    PrintWeights();
 
+    return 0;
+}
+
+
+void PrintWeights()
+{
     int i=n,j=m;
+    cout << "Weights: (1 means taken):\n";
     while( i>0 && j > 0)
     {
         if(matrix[i][j] == matrix[i-1][j])
-        {
-            cout << i << " = 0\n";
             i--;
-        }
+            // cout << i << " = 0\n";
+
         else
         {
             cout << i << " = 1\n";
             j = j - wt[i];
             i--;
         }
-
     }
-
-    return 0;
 }
